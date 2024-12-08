d6b60cf5514fe84a05cd4fe54411599dd4e7dcb4
Emran <emran.marei@gmail.com>
2024-12-08 08:42:03 +0300

    d4 diff sol

--- d4/main.py
+++ d4/main.py
@@ -2,30 +2,36 @@ import sys
 
 word = "XMAS"
 
+
 def get_status(lines, row, col):
     status = {
-        "up": False, "down": False, 
-        "left": False, "right": False,
-        "up-left": False, "up-right": False,
-        "down-left": False, "down-right": False
+        "up": False,
+        "down": False,
+        "left": False,
+        "right": False,
+        "up-left": False,
+        "up-right": False,
+        "down-left": False,
+        "down-right": False,
     }
-    if row >= len(word)-1:
+    if row >= len(word) - 1:
         status["up"] = True
     if row + len(word) <= len(lines):
         status["down"] = True
-    if col >= len(word)-1:
+    if col >= len(word) - 1:
         status["left"] = True
     if col + len(word) <= len(lines[row]):
         status["right"] = True
-    if row >= len(word)-1 and col >= len(word)-1:
+    if row >= len(word) - 1 and col >= len(word) - 1:
         status["up-left"] = True
-    if row >= len(word)-1 and col + len(word) <= len(lines[row]):
+    if row >= len(word) - 1 and col + len(word) <= len(lines[row]):
         status["up-right"] = True
-    if row + len(word) <= len(lines) and col >= len(word)-1:
+    if row + len(word) <= len(lines) and col >= len(word) - 1:
         status["down-left"] = True
     if row + len(word) <= len(lines) and col + len(word) <= len(lines[row]):
         status["down-right"] = True
     return status
+
 
 def part1(lines):
     count = 0
@@ -33,52 +39,67 @@ def part1(lines):
         for col in range(len(lines[row])):
             if lines[row][col] == "X":
                 square_status = get_status(lines, row, col)
-                
+
                 if square_status["up"]:
-                    if "".join([lines[row-i][col] for i in range(len(word))]) == word:
+                    if "".join([lines[row - i][col] for i in range(len(word))]) == word:
                         count += 1
                 if square_status["down"]:
-                    if "".join([lines[row+i][col] for i in range(len(word))]) == word:
+                    if "".join([lines[row + i][col] for i in range(len(word))]) == word:
                         count += 1
                 if square_status["left"]:
-                    if "".join([lines[row][col-i] for i in range(len(word))]) == word:
+                    if "".join([lines[row][col - i] for i in range(len(word))]) == word:
                         count += 1
                 if square_status["right"]:
-                    if "".join([lines[row][col+i] for i in range(len(word))]) == word:
+                    if "".join([lines[row][col + i] for i in range(len(word))]) == word:
                         count += 1
                 if square_status["up-left"]:
-                    if "".join([lines[row-i][col-i] for i in range(len(word))]) == word:
+                    if (
+                        "".join([lines[row - i][col - i] for i in range(len(word))])
+                        == word
+                    ):
                         count += 1
                 if square_status["up-right"]:
-                    if "".join([lines[row-i][col+i] for i in range(len(word))]) == word:
+                    if (
+                        "".join([lines[row - i][col + i] for i in range(len(word))])
+                        == word
+                    ):
                         count += 1
                 if square_status["down-left"]:
-                    if "".join([lines[row+i][col-i] for i in range(len(word))]) == word:
+                    if (
+                        "".join([lines[row + i][col - i] for i in range(len(word))])
+                        == word
+                    ):
                         count += 1
                 if square_status["down-right"]:
-                    if "".join([lines[row+i][col+i] for i in range(len(word))]) == word:
+                    if (
+                        "".join([lines[row + i][col + i] for i in range(len(word))])
+                        == word
+                    ):
                         count += 1
     return count
+
 
 def part2(lines):
     count = 0
     for row in range(1, len(lines) - 1):
         for col in range(1, len(lines[row]) - 1):
             if (
-                (lines[row - 1][col - 1] == "M" and
-                 lines[row][col] == "A" and
-                 lines[row + 1][col + 1] == "S") or
-                (lines[row - 1][col - 1] == "S" and
-                 lines[row][col] == "A" and
-                 lines[row + 1][col + 1] == "M")
+                lines[row - 1][col - 1] == "M"
+                and lines[row][col] == "A"
+                and lines[row + 1][col + 1] == "S"
+            ) or (
+                lines[row - 1][col - 1] == "S"
+                and lines[row][col] == "A"
+                and lines[row + 1][col + 1] == "M"
             ):
                 if (
-                    (lines[row - 1][col + 1] == "M" and
-                     lines[row][col] == "A" and
-                     lines[row + 1][col - 1] == "S") or
-                    (lines[row - 1][col + 1] == "S" and
-                     lines[row][col] == "A" and
-                     lines[row + 1][col - 1] == "M")
+                    lines[row - 1][col + 1] == "M"
+                    and lines[row][col] == "A"
+                    and lines[row + 1][col - 1] == "S"
+                ) or (
+                    lines[row - 1][col + 1] == "S"
+                    and lines[row][col] == "A"
+                    and lines[row + 1][col - 1] == "M"
                 ):
                     count += 1
     return count
--- d4/main2.py
+++ d4/main2.py
@@ -1,18 +1,37 @@
 # another approach for d4
 import sys
 
-#(row, col) direction offset
-directions= [
-    (-1, 0), (0, -1),
-    (0, -1), (0, 1),
-    (-1, -1), (-1, 1,),
-    (1, -1), (1, 1)
+# (row, col) direction offset
+directions = [
+    (-1, 0),
+    (0, -1),
+    (0, -1),
+    (0, 1),
+    (-1, -1),
+    (-1, 1),
+    (1, -1),
+    (1, 1),
 ]
 
+word = "XMAS"
+
+
 def part1(lines):
- # dynamic traversal => include the i of the word and the direction offset
- 
- pass
+    count = 0
+    # dynamic traversal => include the i of the word and the direction offset
+    for row in range(len(lines)):
+        for col in range(len(lines[row])):
+            for dr, dc in directions:
+                try:
+                    sequence = "".join(
+                        lines[row + i * dr][col + i * dc] for i in range(len(word))
+                    )
+                    if sequence == word:
+                        count += 1
+                except IndexError:
+                    pass
+    return count
+
 
 if __name__ == "__main__":
     args = sys.argv[1:]

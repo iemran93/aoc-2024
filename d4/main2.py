ba440351eb2fd2d1c52965f41036dabac664bd13
Emran <emran.marei@gmail.com>
2024-12-09 12:14:37 +0300

    d4 another approach

--- d4/main2.py
+++ d4/main2.py
@@ -4,7 +4,7 @@ import sys
 # (row, col) direction offset
 directions = [
     (-1, 0),
-    (0, -1),
+    (1, 0),
     (0, -1),
     (0, 1),
     (-1, -1),
@@ -16,21 +16,39 @@ directions = [
 word = "XMAS"
 
 
-def part1(lines):
+def sol(lines):
     count = 0
+    count2 = 0
     # dynamic traversal => include the i of the word and the direction offset
     for row in range(len(lines)):
         for col in range(len(lines[row])):
+            if lines[row][col] == "A":
+                # boundry checking
+                if 1 <= row < len(lines) - 1 and 1 <= col < len(lines[row]) - 1:
+                    diag1 = (
+                        lines[row - 1][col - 1]
+                        + lines[row][col]
+                        + lines[row + 1][col + 1]
+                    ) in ["MAS", "SAM"]
+                    diag2 = (
+                        lines[row - 1][col + 1]
+                        + lines[row][col]
+                        + lines[row + 1][col - 1]
+                    ) in ["MAS", "SAM"]
+                    if diag1 and diag2:
+                        count2 += 1
+
             for dr, dc in directions:
-                try:
+                if 0 <= row + (len(word) - 1) * dr < len(lines) and 0 <= col + (
+                    len(word) - 1
+                ) * dc < len(lines[row]):
                     sequence = "".join(
                         lines[row + i * dr][col + i * dc] for i in range(len(word))
                     )
                     if sequence == word:
                         count += 1
-                except IndexError:
-                    pass
-    return count
+
+    return count, count2
 
 
 if __name__ == "__main__":
@@ -46,5 +64,5 @@ if __name__ == "__main__":
     with open(data_path, "r") as file:
         for line in file:
             lines.append(line.strip())
-    print("Part1 solution: ", part1(lines))
-    # print("Part2 solution: ", part2(lines))
+    p1, p2 = sol(lines)
+    print(f"p1 sol: {p1}\np2 sol: {p2}\n")
